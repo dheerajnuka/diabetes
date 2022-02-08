b55117c76f11b0c43b654d9bf239c225f19dad77
dheerajnuka <dheerajnuka1996@gmail.com>
2022-02-08 17:38:49 +0530

    Dheeraj Commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 import sqlite3
+import matplotlib as plt
 # Load the Random Forest CLassifier model
 filename = 'diabetes-prediction-rfc-model.pkl'
 classifier = pickle.load(open(filename, 'rb'))
